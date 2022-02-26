94ef365331ed282ccf47f1ed0b1868c9be2d5970
Jarvis <jignesh07@gmail.com>
2022-02-26 14:23:58 +0530

    FrontEnd

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div>
+
+      <Home/>
+      <Login/>
+      
+    </div>
+  );
+}
+
+export default App;
